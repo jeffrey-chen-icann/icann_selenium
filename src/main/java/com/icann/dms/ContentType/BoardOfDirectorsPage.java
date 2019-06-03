a3b2476475b033cb5d06af843ce926fb91bc97b8
Jeff Chen <jeffrey.chen@JECH-8349.local>
2019-06-03 16:38:27 -0700

    + new files and jar

---
+++ src/main/java/com/icann/dms/ContentType/BoardOfDirectorsPage.java
@@ -0,0 +1,13 @@
+package com.icann.dms.contenttype;
+
+import org.openqa.selenium.*;
+import com.icann.Helper;
+import com.icann.dms.ContentItem;
+
+public class BoardOfDirectorsPage extends ContentItem {
+	static WebDriver browser = Helper.browser;
+	
+    public static By txtPageTitle = By.id("icn:pageTitle");
+    public static By pageDescriptionExpander = By.id("field-htmlblock1-container");
+
+}
