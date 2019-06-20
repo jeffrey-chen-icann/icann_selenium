cbaf419aea2890d32566fa05e1860ea3381c1e42
Jeff Chen <jeffrey.chen@JECH-8349.local>
2019-06-20 09:34:20 -0700

    added Metadata page object

---
+++ src/main/java/com/icann/_contentrecordtypes/Metadata.java
@@ -0,0 +1,8 @@
+package com.icann._contentrecordtypes;
+
+public class Metadata {
+	public String sAgreementDate = "";
+	public String sAgreementStatus = "";
+	public String sMetadataDescription = "";
+
+}
